78f717b678351aad52384dca19e95898335cb305
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-04-29 22:30:36 +0200

    Creata classe di gestione
    Punto di ingresso del programma nella classe ServerManagerCLI. Completare con menu a scelta per controllare il server

--- src/server/backend/beam/comm/WebServer.java
+++ src/server/backend/beam/comm/WebServer.java
@@ -11,38 +11,38 @@ import java.io.*;
  * @author franc
  *
  */
-public class Server implements Runnable {
-	
+public class WebServer implements Runnable {
+
 	/**
 	 * Unica istanza della classe (singleton)
 	 */
-	private static Server me;
-	
+	private static WebServer me;
+
 	/**
 	 * Porta di apertura del servizio
 	 */
 	public static final int PORT = 80;
-	
+
 	/**
 	 * {@link ServerSocket} esporre il server sulla rete
 	 */
 	private ServerSocket server;
-	
+
 	/**
 	 * Array di {@link ClientHandler} per la gestione di più clients
 	 */
 	private ArrayList<ClientHandler> clientHandlers;
-	
+
 	/**
 	 * True per interrompere l'attesa di nuove connessioni
 	 */
 	private boolean acceptingStopped;
-	
+
 	/**
 	 * Referenze a pagine web salvate sul server
 	 */
 	private File[] pagine;
-	
+
 	public File[] getPagine() {
 		return pagine;
 	}
@@ -54,73 +54,73 @@ public class Server implements Runnable {
 	 * Cartella origine in cui sono salvate tutte le pagine web
 	 */
 	public static final String SERVER_ROOT = new File("").getAbsolutePath() + "\\root";//indirizzo
-	
+
 	/**
 	 * Ritorno dell'unica istanza della classe {@link Server}
 	 * @return
 	 * @throws IOException
 	 */
-	public static Server getInstance() throws IOException {
+	public static WebServer getInstance() throws IOException {
 		if(me == null) {
 			try {
-				me = new Server();
+				me = new WebServer();
 			} catch (IOException e) {
 				throw e;
 			}
 		}
 		return me;
 	}
-	
+
 	/**
 	 * Costruttore privato per Singleton
 	 * @throws IOException 
 	 */
-	private Server() throws IOException {
+	private WebServer() throws IOException {
 		try {
 			server = new ServerSocket(PORT);
 			pagine = caricaPagine();
-			
+
 		} catch (IOException e) {
 			throw e;
 		}
 		clientHandlers = new ArrayList<ClientHandler>();
 		acceptingStopped = false;
-		
+
 	}
-	
+
 	/**
 	 * Dalla cartella di root del server carica tutte le pagine disponibili
 	 * @return
 	 */
 	public File[] caricaPagine() {
 		File root = new File(SERVER_ROOT);
-		
+
 		return root.listFiles(new PageFileFilter());
-		
+
 	}
-	
+
 	/**
 	 * Attende una connessione da parte di un client
 	 * @return {@link Socket} di collegamento col client
 	 */
 	public Socket attendi() {
-		
-			
+
+
 		Socket res = null;
-		
+
 		try {
-			System.out.println("Pronto, in ascolto sulla porta "+PORT);
+			System.out.println("\t\t\tPronto, in ascolto sulla porta "+PORT);
 			res = server.accept();
-			System.out.println("Connessione stabilita ");
+			System.out.println("\t\t\tConnessione stabilita ");
 		} catch (IOException e) {
-			
+
 		}
-		
-		
+
+
 		return res;
-		
+
 	}
-	
+
 	@Override
 	public void run() {
 		while(!acceptingStopped) {
@@ -128,15 +128,15 @@ public class Server implements Runnable {
 			new Thread(cl).start();
 			clientHandlers.add(cl);
 		}
-		
+
 		try {
 			server.close();
 		} catch (IOException e) {
-			
+
 		}
-		
+
 	}
-	
+
 
 	/**
 	 * 
@@ -145,7 +145,7 @@ public class Server implements Runnable {
 	public ServerSocket getServer() {
 		return server;
 	}
-	
-	
+
+
 
 }
